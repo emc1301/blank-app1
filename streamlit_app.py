2561c7679d69a006e97762f818f1846b0933f0b2
emc1301 <ella.campbell@emcai.co.uk>
2024-08-24 13:50:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Ella's App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "I hope this works."
 )
 
 import streamlit as st
